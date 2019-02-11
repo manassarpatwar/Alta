a3042b2247021989461481e5bbbc668fa06cadeb
AquinasAhoy <agcdoctor@gmail.com>
2019-02-11 15:27:16 +0000

    twitterTest Initial

---
+++ twitterTest/testTwitter.rb
@@ -0,0 +1,11 @@
+require 'twitter'
+
+config = {
+	:consumer_key => 'DobOnYsBxhupb4tWtkIkud2nY',
+	:consumer_secret => 'rNFlJpJv0b0gSYtrzgKfKcyEMM2L6AtwgsHgv3WIuqg6InXq8w',
+	:access_token => '1092444312430919681-5lkYoGwZcwwDZbzoGUbZwtRQ1ISZV8',
+	:access_token_secret => '78VHTUvHE4B7OO8Tv6xHYkkkuE4rWqB0bdGjcpNUYvleh'
+}
+
+client = Twitter::REST::Client.new(config)
+client.update('This is Tom sending an automated tweet using Ruby')
