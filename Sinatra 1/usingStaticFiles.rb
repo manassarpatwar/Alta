af7b95004438e4b45629cf216ab1de2fcbc92c42
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2019-03-04 16:33:24 +0000

    Tweaked admin dashboard

--- Sinatra 1/usingStaticFiles.rb
+++ Sinatra 1/usingStaticFiles.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 set :bind, '0.0.0.0' # Only needed if you're running from Codio
-
 get '/' do
     erb :usingStaticFiles
 end
--- test.rb
+++ test.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require 'twitter'
+config = { 
+    :consumer_key => '0gz9YqiS7VD6kgNJTkeVhjnd7', 
+    :consumer_secret => 'GEmoZ61BNX8BRXcS2dGwZZ36BIsqKvV1HXlB5v6iOOlSnvif5U', 
+    :access_token => '1092444312430919681-7Wfb7Bymkw5i7t1RQc2Lwip0IhFSzR', 
+    :access_token_secret => 'nlDgQf8Cx0wYMhgFQnp1WgaflRt6nlmyO869W4Kxsit88' 
+    }
+client = Twitter::REST::Client.new(config)
+client.search("to:uber", result_type: "recent").take(20).each do |tweet|
+      puts tweet.full_text
+end
 
-get '/test' do
+get '/dashboard' do
+  erb :dashboard
+end
+
+get '/index' do
   erb :index
 end
