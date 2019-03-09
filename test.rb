b3a68ff7ad4f0504fdd9960ecd168cda19612a9a
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2019-03-09 12:17:08 +0000

    Updated config keys

--- test.rb
+++ test.rb
@@ -9,16 +9,15 @@ include ERB::Util
 
 before do
   config = {
-    :consumer_key => '6VCt8o2esXR3JoWYSIqxjlfEE',
-    :consumer_secret => 'Cjj1zX9T99xKqb8BFO9WQPjco6WbBpDXNUbaN4Y68ZZJrNU9nL',
-    :access_token => '1092444312430919681-g4S8YlnAD6vaqZ63YU7O7n1vS26O2O',
-    :access_token_secret => 'Hrkx1ZicwUNzRsoKMEPUAMaUqd2GRKY1sawCNRzcL414q'
+    :consumer_key => 'wVzUO14M25jvS3vmmtfDAtmh6',
+    :consumer_secret => 'x1hieq7QNwhbUM8wjqgl5HujELyyqmZiJUzpaWi1tQEnG8cQrX',
+    :access_token => '1092444312430919681-k6yytElynjt9A1ziskr28eHKLg580X',
+    :access_token_secret => 'UkK1okCoI1kFUKeofvh5Y5QQHkJyVOQxeIQGQfyCjIFQP'
   }
   @client = Twitter::REST::Client.new(config)
 end
 
 get '/dashboard' do
-   helper_method :fetch_tweets
     def fetch_tweets
       results = @client.search("to:uber", result_type: "recent", lang: "en", geocode: "53.3,-1.5,1000km").take(20)
       @tweets = @client.oembeds(results, hide_media: "true", hide_thread: "true")
