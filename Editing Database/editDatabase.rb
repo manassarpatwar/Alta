c0b0b4ab7a49ec5db17545bbb6e624f0ec7954dd
AquinasAhoy <agcdoctor@gmail.com>
2019-03-16 12:32:40 +0000

    Taxis added

--- Editing Database/editDatabase.rb
+++ Editing Database/editDatabase.rb
@@ -7,7 +7,7 @@ set :bind, '0.0.0.0' # Only needed if you're running from Codio
 include ERB::Util
 
 before do
-	@db = SQLite3::Database.new('./editDatabase.sqlite')
+	@db = SQLite3::Database.new('./taxi_database.sqlite')
 end
 
 get '/add' do
@@ -19,25 +19,40 @@ post '/add' do
 	@submitted = true
 
 	# sanitize values
-	@city = params[:city].strip
-	@country = params[:country].strip
-	@population = params[:population].strip
+	@id = params[:id].strip
+	@name = params[:name].strip
+	@dateTime = params[:dateTime].strip
+	@userType = params[:userType].strip
+	@freeRide = params[:freeRide].strip
 
 	# perform validation
-	@city_ok = !@city.nil? && @city != ""
-	@population_ok = @population =~ /^[0-9]*$/
-	count = @db.get_first_value('SELECT COUNT(*) FROM cities WHERE city = ? AND country = ?', [@city, @country])
-	@unique = (count == 0)
+	@id_ok = !@id.nil? && @id != ""
+	@name_ok = !@name.nil? && @name != ""
+	@dateTime_ok = !@dateTime.nil? && @dateTime != ""
+	@userType_ok = @userType = "1" || @userType = "0"
+	@freeRide_ok = isPositiveNumber?(@freeRide)
+
+	#count = @db.get_first_value('SELECT COUNT(*) FROM cities WHERE city = ? AND country = ?', [@city, @country])
+	#@unique = (count == 0)
 	
-	@all_ok = @city_ok && @population_ok && @unique
+	@all_ok = @city_ok && @name_ok && @dateTime_ok && @userType_ok && @freeRide_ok
 
   	# add data to the database
 	if @all_ok
-    	# get next available ID
-		id = @db.get_first_value 'SELECT MAX(id)+1 FROM cities';
-
     	# do the insert
-		@db.execute('INSERT INTO users VALUES (?, ?, ?, ?)', [id, @city, @country, @population])
+		@db.execute('INSERT INTO users VALUES (?, ?, ?, ?, ?)', [@id, @name, @dateTime, @userType.to_i, @freeRide.to_i])
   	end
 	erb :add
+end
+
+def isPositiveNumber? string
+	if Integer(string) rescue false
+		if string.to_i >= 0
+			return true
+		else
+			return false
+		end
+	else
+		return false
+	end
 end
