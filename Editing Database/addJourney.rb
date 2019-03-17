8ed31585ce21553f5851433aa6074dd2510a7407
AquinasAhoy <agcdoctor@gmail.com>
2019-03-17 16:48:44 +0000

    Fixed addJourney.rb

--- Editing Database/addJourney.rb
+++ Editing Database/addJourney.rb
@@ -32,17 +32,17 @@ post '/add' do
 
 	# perform validation
 	#taxiID and userID and twitterHandle needs better validation!!!!!
-	@taxiId_ok = isPositiveNumber?[@taxiId]
-	@userId_ok = !@userID.nil? && @userID != ""
+	@taxiId_ok = isPositiveNumber?(@taxiId)
+	@userId_ok = !@userId.nil? && @userId != ""
 	@twitterHandle_ok = !@twitterHandle.nil? && @twitterHandle != ""
 	@dateTime_ok = !@dateTime.nil? && @dateTime != ""
 	@startLocation_ok = !@startLocation.nil? && @startLocation != ""
 	@endLocation_ok = !@endLocation.nil? && @endLocation != ""
-	@freeRide_ok = @freeRide = '0' || @ freeRide = '1'
+	@freeRide_ok = @freeRide = '0' || @freeRide = '1'
  	@cancelled_ok = @cancelled = '0' || @cancelled = '1'
 	@convoLink_ok =	!@convoLink.nil? && @convoLink != ""
 	
-	@all_ok = @taxiId_ok && @userId_ok && @twitterHandle_ok && dateTime_ok && startLocation_ok && endLocation_ok && freeRide_ok && cancelled_ok && convoLink_ok	
+	@all_ok = @taxiId_ok && @userId_ok && @twitterHandle_ok && @dateTime_ok && @startLocation_ok && @endLocation_ok && @freeRide_ok && @cancelled_ok && @convoLink_ok	
 
 	count = @db.get_first_value('SELECT COUNT(*) FROM journeys')
 	@id = count + 1
