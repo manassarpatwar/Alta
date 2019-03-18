32fa3a4f26051f8a088e76ef329834adb53af537
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2019-03-18 17:31:24 +0000

    Dashboard add journey invalid form attributes fixed via ajax post

--- app.rb
+++ app.rb
@@ -7,15 +7,12 @@ require 'twitter'
 require 'rufus-scheduler'
 require 'omniauth-twitter'
 require 'sqlite3'
-require_relative 'addJourney.rb'
 require_relative 'addTaxi.rb'
 require_relative 'addUser.rb'
-require_relative 'addComplaint.rb'
 require_relative 'dashboard.rb'
 require_relative 'main.rb'
 require_relative 'login.rb'
 require_relative 'complaint.rb'
-require_relative 'marketing.rb'
 
 
 set :bind, '0.0.0.0' # Needed when running from Codio
--- dashboard.rb
+++ dashboard.rb
@@ -1,9 +1,8 @@
 #--------------------Get Methods--------------------#
 get '/dashboard' do
-    redirect '/' unless admin?
+    #redirect '/' unless admin?
     @tweets = $tweets.dup
     @submitted = false
-	erb :addJourney
 	erb :dashboard
 end
 
@@ -31,4 +30,60 @@ post '/delete_tweet' do
   $tweets.delete($tweets[index])
   $tweets =  TWITTER_CLIENT.mentions_timeline(count: "1", since_id: "#{$since_id}") + $tweets
   redirect '/dashboard'
+end
+
+post '/addJourney' do
+	@submitted = true
+    puts "I Am IN POST ADD JOURNEY!!!!"
+	# sanitize values
+	@taxiId = params[:taxiId].strip
+	@userId = params[:userId].strip
+	@twitterHandle = params[:twitterHandle].strip
+	@dateTime = params[:dateTime].strip
+	@startLocation = params[:startLocation].strip
+	@endLocation = params[:endLocation].strip
+	@freeRide = params[:freeRide].strip
+	@cancelled = params[:cancelled].strip
+	@rating = params[:rating].strip
+	@convoLink = params[:convoLink].strip
+
+	# perform validation
+	#taxiID and userID and twitterHandle needs better validation!!!!!
+	@taxiId_ok = isPositiveNumber?(@taxiId)
+	@userId_ok = !@userId.nil? && @userId != ""
+	@twitterHandle_ok = !@twitterHandle.nil? && @twitterHandle != ""
+	@dateTime_ok = !@dateTime.nil? && @dateTime != ""
+	@startLocation_ok = !@startLocation.nil? && @startLocation != ""
+	@endLocation_ok = !@endLocation.nil? && @endLocation != ""
+	@freeRide_ok = @freeRide = '0' || @freeRide = '1'
+ 	@cancelled_ok = @cancelled = '0' || @cancelled = '1'
+	@convoLink_ok =	!@convoLink.nil? && @convoLink != ""
+	
+	@all_ok = @taxiId_ok && @userId_ok && @twitterHandle_ok && @dateTime_ok && @startLocation_ok && @endLocation_ok && @freeRide_ok && @cancelled_ok && @convoLink_ok	
+
+	count = @db.get_first_value('SELECT COUNT(*) FROM journeys')
+	@id = count + 1
+
+  	# add data to the database
+	if @all_ok
+    	# do the insert
+		@db.execute('INSERT INTO journeys VALUES (?, ?, ?, ?, ?, ? ,?, ?, ?, ?, ?)', [@id, @taxiId, @userId, @twitterHandle, @dateTime, @startLocation, @endLocation, @freeRide, @cancelled, @rating, @convoLink])
+  	end
+    erb :addJourney
+end
+
+def isPositiveNumber? string
+	begin
+		Float(string)
+		int = string.to_i 
+		puts "Not fail"
+		if int >= 0
+			return true
+		else
+			return false
+		end
+	rescue 
+		puts "fail"
+		return false
+	end
 end
