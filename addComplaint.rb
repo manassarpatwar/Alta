4f0c27d9373c9a70ef3222db41d416313f966d27
Tom Tonner <tbtonner1@sheffield.ac.uk>
2019-03-22 12:29:49 +0000

    Fixed spacing error

--- addComplaint.rb
+++ addComplaint.rb
@@ -1,25 +1,25 @@
 post '/addComplaint' do
     @submitted = true
-    
+
     #sanitize values
-    @journey_id = params[: journey_id]
-    @user_id = params[: user_id]
+    @journey_id = params[:journey_id]
+    @user_id = params[:user_id]
     @date_time = Time.now.strftime("%d/%m/%Y %H:%M").to_s
-    @complaint = params[: complaint]
-    
+    @complaint = params[:complaint]
+
     # perform validation
-    @journey_id_ok = isPositiveNumber ? (@journey_id)
-    @complaint_ok = !@complaint.nil ? && @complaint != ""
-    @user_id_ok = !@user_id.nil ? && @user_id != ""
-    
+    @journey_id_ok = isPositiveNumber? (@journey_id)
+    @complaint_ok = !@complaint.nil? && @complaint != ""
+    @user_id_ok = !@user_id.nil? && @user_id != ""
+
     @all_ok = @journey_id_ok && @complaint_ok && @user_id_ok
-    
+
     count = $db.get_first_value('SELECT COUNT(*) FROM complaints')
     @id = count
-    
+
     # add data to the database
     if @all_ok# do the insert
         $db.execute('INSERT INTO complaints VALUES (?, ?, ?, ?, ?)', [@id, @journey_id, @user_id, @date_time, @complaint])
     end
-    erb: addComplaint
+    erb :addComplaint
 end
--- addTaxi.rb
+++ addTaxi.rb
@@ -1,26 +1,26 @@
 post '/addTaxi' do
     @submitted = true
-    
+
     #sanitize values
-    @regNum = params[: regNum].strip
-    @contact = params[: contact].strip
-    @taxiType = params[: taxiType].strip.upcase# perform validation
-    @regNum_ok = !@regNum.nil ? && @regNum != ""
-    @contact_ok = !@contact.nil ? && @contact != ""
+    @regNum = params[:regNum].strip
+    @contact = params[:contact].strip
+    @taxiType = params[:taxiType].strip.upcase# perform validation
+    @regNum_ok = !@regNum.nil? && @regNum != ""
+    @contact_ok = !@contact.nil? && @contact != ""
     if @taxiType == "S" || @taxiType == "M" || @taxiType == "L"
         @taxiType_ok = true
     else
         @taxiType_ok = false
     end
-    
+
     @all_ok = @regNum_ok && @contact_ok && @taxiType_ok
-    
+
     count = $db.get_first_value('SELECT COUNT(*) FROM taxis')
     @id = count + 1
-    
+
     # add data to the database
     if @all_ok# do the insert
         $db.execute('INSERT INTO taxis VALUES (?, ?, ?, ?)', [@id, @regNum, @contact, @taxiType])
     end
-    erb: addTaxi
+    erb :addTaxi
 end
