901fd974f184ea903c26cc079a44857b3af75e07
Simonas Petkevicius <spetkevicius1@sheffield.ac.uk>
2019-03-22 12:23:03 +0000

    adding scheduler.rb

---
+++ scheduler.rb
@@ -0,0 +1,38 @@
+require 'rufus-scheduler'
+require 'sinatra'
+require 'twitter'
+
+configure do
+     autoConfig = {
+        :consumer_key => 'OH20bwn5ml5a6J7cpWAfn44sw',
+        :consumer_secret => '20cW4vtmfWNTbcXf5XrCAlWtGktFHXOBGafK1RwImOQCI473XN',
+        :access_token => '1092444312430919681-04XWeN319ppuOGPy6Qtdpz4t0AwcVB',
+        :access_token_secret => '4B00DH6sUPvugxshMQjmmaYPTlnQM4ftpUOgcyoDaGfYq'
+    }
+    @clientAutomaticFollowing = Twitter::REST::Client.new(autoConfig)
+end
+
+def following_scheduler()
+    scheduler = Rufus::Scheduler.new(:frequency => '30s')
+
+    scheduler.every("1m") do
+        puts ("loop started scheduler")
+        begin
+            puts ("1")
+            mentions = @clientAutomaticFollowing.mentions_timeline()
+            puts ("2")
+            most_recent = mentions.take(5)
+            puts ("3")
+            most_recent.each do |tweet|
+                puts ("4")
+                @clientAutomaticFollowing.follow(tweet.user.screen_name)
+            end
+            puts ("Automated following initiated")
+        rescue Twitter::Error::TooManyRequests => error
+            sleep error.rate_limit.reset_in
+            puts ("To many requests to twitter API scheduler.rb line 30 before do")
+        end
+    end
+    puts("started Scheduler #{scheduler.object_id}")
+    #response.set_cookie(:automated_following, :value => "false")
+end
