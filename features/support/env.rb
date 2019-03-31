457195fe75a42ee2cadac53faf8e0edba5b99fa6
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2019-03-31 23:38:44 +0100

    Cucumber tests can now run on codio provided chromedriver is copied onto /bin
    Further changes
    -On adding a journey taxi becomes unavailable.
    -Home page now is stutter and lag free

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,23 +2,26 @@ require 'capybara'
 require 'rspec'
 require 'capybara/cucumber'
 require 'capybara-screenshot/cucumber'
+require 'selenium-webdriver'
 ## Uncomment to enable SimpleCov
-require 'simplecov'
+#require 'simplecov'
 
-SimpleCov.start do
-  add_filter 'features/'
-end
+#SimpleCov.start do
+#  add_filter 'features/'
+#end
 
 require_relative '../../app.rb'
 
 ENV['RACK_ENV'] = 'test'
 
 Capybara.app = Sinatra::Application
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless window-size=1280,960 remote-debugging-port=9222] }
+  )
+  Capybara::Selenium::Driver.new(app, browser: :headless_chrome, desired_capabilities: capabilities)
 end
-
-Capybara.javascript_driver = :chrome
+Capybara.javascript_driver = :selenium_chrome_headless
 class Sinatra::ApplicationWorld
   include RSpec::Expectations
   include RSpec::Matchers
