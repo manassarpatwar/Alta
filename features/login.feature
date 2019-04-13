d369014d07608928a612e72ec16a85b31dc67728
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2019-04-14 00:07:21 +0100

    Tests for logging in as admin

---
+++ features/login.feature
@@ -0,0 +1,9 @@
+Feature: Testing logging in
+       @javascript
+       Scenario: Testing go to login page and sign in
+              Given I am on the login page
+              When I fill in "Username or email" with "ise19team29"
+              When I fill in "Password" with "SoftEng2019"
+              When I press "Sign In"
+              And I am on the dashboard page
+              Then I should see "Taxis"
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,18 +3,20 @@ require 'rspec'
 require 'capybara/cucumber'
 require 'capybara-screenshot/cucumber'
 require 'selenium-webdriver'
-## Uncomment to enable SimpleCov
+
 #require 'simplecov'
 
 #SimpleCov.start do
 #  add_filter 'features/'
 #end
-
 require_relative '../../app.rb'
 
 ENV['RACK_ENV'] = 'test'
 
 Capybara.app = Sinatra::Application
+Capybara.app_host = 'http://localhost:4567'
+Capybara.server_port = 4567
+
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     chromeOptions: { args: %w[headless window-size=1280,960 remote-debugging-port=9222] }
@@ -22,6 +24,11 @@ Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :headless_chrome, desired_capabilities: capabilities)
 end
 Capybara.javascript_driver = :selenium_chrome_headless
+#Set to chromedriver to see realtime tests
+#Capybara.register_driver :chrome do |app|
+#  Capybara::Selenium::Driver.new(app, browser: :chrome)
+#end
+#Capybara.javascript_driver = :chrome
 class Sinatra::ApplicationWorld
   include RSpec::Expectations
   include RSpec::Matchers
