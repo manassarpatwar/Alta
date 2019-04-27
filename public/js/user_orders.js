1ea52090afa1da0d7d907f1cbb930a15ea3403ce
coa18cyy <cyyuen2@sheffield.ac.uk>
2019-04-27 22:29:13 +0100

    user order page)) Delete Account button
    Table view of user history.
    Delete Account Button.

--- public/js/user_orders.js
+++ public/js/user_orders.js
@@ -520,3 +520,16 @@ window.addEventListener ?
 	});
 
 
+
+	//delete account
+
+	var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
