2dc772c29814421c8c1ff7deab17322167e65d23
coa18cyy <cyyuen2@sheffield.ac.uk>
2019-04-27 18:36:20 +0100

    Table with dropdown list filtering
    Is now able to filter table with 1. dropdownlists selection togther with the 2.input from user.

--- table.rb
+++ table.rb
@@ -4,66 +4,36 @@ set :bind, '0.0.0.0' # Needed when running from Codio
 include ERB::Util #Ensure ERB is enabled   
 
 
-
-# DB = Sequel.sqlite
-# DB2 = Sequel.connect('sqlite://./taxi_database.sqlite')
-
-# DB2.create_table :test do
-#     Integer :id, :journeys
-#     Integer :date
-#     Integer :start_location
-#     Integer :end_location
-#     unique [:id, :id]
-#     # unique [:date, :date_time]
-#     # unique [:start_location, :start_location]
-#     # unique [:end_location, :end_location]
-# end
-
-# DB2[:test].each do |row|
-#     p row
-#   end
-
-# DB.create_table :people do
-#   primary_key :id
-#   String :name
-#   Integer :date_time
-# end
-
-# DB.create_table :items do
-#   foreign_key :person_id, :people
-#   String  :name
-#   Integer :quantity, default: 1
-#   unique  [:person_id, :name]
-# end
-
-# stacey_id = DB[:people].insert(name: "Stacey", date_time: "1")
-# avdi_id   = DB[:people].insert(name: "Avdi", date_time: "1")
-
-# puts DB[:people].count #2
-# puts DB[:people].all
-# # => [{:id=>1, :name=>"Stacey"}, {:id=>2, :name=>"Avdi"}]]
-
-# DB[:items].insert(avdi_id, "Pie", 2)
-# DB[:items].insert(avdi_id, "Mineral Water", 1)
-# DB[:items].insert(stacey_id, "Heads of Kale", 3)
-# DB[:items].insert(stacey_id, "Mineral Water", 2)
-
-# DB[:items].each do |row|
-#     p row
-#   end
-
-
-
-
 get'/userOrders' do
     @db = SQLite3::Database.new './taxi_database.sqlite'
 
-        query = %{SELECT * FROM journeys WHERE id LIKE '%#{h params[:search]}%' }
-        # query = %{SELECT id, date_time, start_location, end_location, free_ride, cancelled, rating
-        #             FROM journeys
-        #             WHERE id LIKE '%#{params[:search]}%'}
+    if params[:column] == 'all'
+        query = %{SELECT * FROM journeys}
         @results = @db.execute query
+    elsif params[:column] == 'id'   
+        query = %{SELECT * FROM journeys WHERE id LIKE '%#{h params[:search]}%' }
+        @results = @db.execute query
+    elsif params[:column] == 'date_time' 
+        query = %{SELECT * FROM journeys WHERE date_time LIKE '%#{h params[:search]}%' }
+        @results = @db.execute query
+    elsif params[:column] == 'start_location' 
+        query = %{SELECT * FROM journeys WHERE start_location LIKE '%#{h params[:search]}%' }
+        @results = @db.execute query
+    elsif params[:column] == 'end_location' 
+        query = %{SELECT * FROM journeys WHERE end_location LIKE '%#{h params[:search]}%' }
+        @results = @db.execute query
+    elsif params[:column] == 'free_ride' 
+        query = %{SELECT * FROM journeys WHERE free_ride LIKE '%#{h params[:search]}%' }
+        @results = @db.execute query
+    elsif params[:column] == 'cancelled' 
+        query = %{SELECT * FROM journeys WHERE cancelled LIKE '%#{h params[:search]}%' }
+        @results = @db.execute query
+    elsif params[:column] == 'rating' 
+        query = %{SELECT * FROM journeys WHERE rating LIKE '%#{h params[:search]}%' }
+        @results = @db.execute query
+    else
+        puts 'Wrong'
+    end
     erb :user_orders
 end
-
-
+    
