5593905761f57bbef9fe3c440c5b92c24cb758fa
coa18cyy <cyyuen2@sheffield.ac.uk>
2019-04-28 14:50:08 +0100

    User order table + page
    1. Page tables are linked to users' twitterhandle
    2. Enable user to choose according to ride types (all, paid, cancelled, free)
    3. Display all records when no filter are chosen (also already been displayed when logged in)
    4. Search+filter bar fixed

--- table.rb
+++ table.rb
@@ -7,36 +7,49 @@ include ERB::Util #Ensure ERB is enabled
 get'/userOrders' do
     @db = SQLite3::Database.new './taxi_database.sqlite'
 
-    if params[:column].nil?
-        query = %{SELECT * FROM journeys}
+    if params[:column].nil? && !params[:allAlltype] && !params[:paidtype] && !params[:freetype] && !params[:cancelledtype]
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%'}
         @results = @db.execute query
     elsif params[:column] == 'all'
-        query = %{SELECT * FROM journeys}
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%'}
         @results = @db.execute query
     elsif params[:column] == 'id'   
-        query = %{SELECT * FROM journeys WHERE id LIKE '%#{h params[:search]}%' }
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%' AND id LIKE '%#{h params[:search]}%'}
         @results = @db.execute query
     elsif params[:column] == 'date_time' 
-        query = %{SELECT * FROM journeys WHERE date_time LIKE '%#{h params[:search]}%' }
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%' AND date_time LIKE '%#{h params[:search]}%' }
         @results = @db.execute query
     elsif params[:column] == 'start_location' 
-        query = %{SELECT * FROM journeys WHERE start_location LIKE '%#{h params[:search]}%' }
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%' AND start_location LIKE '%#{h params[:search]}%' }
         @results = @db.execute query
     elsif params[:column] == 'end_location' 
-        query = %{SELECT * FROM journeys WHERE end_location LIKE '%#{h params[:search]}%' }
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%' AND end_location LIKE '%#{h params[:search]}%' }
         @results = @db.execute query
     elsif params[:column] == 'free_ride' 
-        query = %{SELECT * FROM journeys WHERE free_ride LIKE '%#{h params[:search]}%' }
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%' AND free_ride LIKE '%#{h params[:search]}%' }
         @results = @db.execute query
     elsif params[:column] == 'cancelled' 
-        query = %{SELECT * FROM journeys WHERE cancelled LIKE '%#{h params[:search]}%' }
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%' AND cancelled LIKE '%#{h params[:search]}%' }
         @results = @db.execute query
     elsif params[:column] == 'rating' 
-        query = %{SELECT * FROM journeys WHERE rating LIKE '%#{h params[:search]}%' }
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%' AND rating LIKE '%#{h params[:search]}%' }
+        @results = @db.execute query
+    elsif params[:allAlltype]
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%'}
+        @results = @db.execute query
+    elsif params[:paidtype]
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%' AND free_ride LIKE 0}
+        @results = @db.execute query
+    elsif params[:freetype]
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%' AND free_ride LIKE 1}
+        @results = @db.execute query
+    elsif params[:cancelledtype]
+        query = %{SELECT * FROM journeys WHERE twitter_handle = '%#{h session[:name]}%' AND cancelled LIKE 1}
         @results = @db.execute query
     else
-        puts 'Wrong'
+        puts 'Error1'
     end
     erb :user_orders
 end
-    
+
+
