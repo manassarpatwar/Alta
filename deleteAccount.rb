bc1588120feef2758da7f86f6339698e65ebe998
Ken Chan <klchan2@sheffield.ac.uk>
2019-04-29 16:07:10 +0000

    Delete account done-needed refinement on it aka Design

--- deleteAccount.rb
+++ deleteAccount.rb
@@ -1,33 +1,6 @@
-class deleteMyAccount
-    
-    
-   def delete 
-       @found = false
-       
-       @usersTable = $db.execute %{SELECT * FROM users} #Gather all user data
-        
-        @usersTable.each do |record| #Go through each user record
-            if env['omniauth.auth']['uid'] == record[0] #If uid = id then:
-                @found = true #Boolean found is true (record is already there)
-            end
-        end
-       
-       if @found
-           $db.execute("DELETE FROM users WHERE id LIKE "env['omniauth.auth']['uid']" )
-           $db.execute("DELETE FROM journeys WHERE user_id LIKE "env['omniauth.auth']['uid']" )
-           $db.execute("DELETE FROM complaints WHERE user_id LIKE "env['omniauth.auth']['uid']" )
-       end
-    end   
-    
-    get '/' do
-        erb :home
-    end
-
-    post '/runMethod' do
-        delete
-        redirect '/'
-    end
-       
-       
-       
-   end
+# delete account
+post '/deleteAccount/' do
+	#execute the deletion
+	$db.execute("DELETE FROM users WHERE id='#{session[:id]}'")
+	redirect '/'
+end
