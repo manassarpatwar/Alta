12b233dd13728a72a74d7400af46aa1d50ad6b79
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2019-04-29 18:29:37 +0100

    Optimized get data from user in userorders

--- main.rb
+++ main.rb
@@ -16,34 +16,24 @@ end
 
 get'/userOrders' do
     redirect '/index' unless session[:loggedin]
-    if params[:column].nil? && !params[:allAlltype] && !params[:paidtype] && !params[:freetype] && !params[:cancelledtype]
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id =  '#{session[:id]}'")
-    elsif params[:column] == 'all'
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id =  '#{session[:id]}'")
-    elsif params[:column] == 'id'   
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND id LIKE '%#{params[:search].strip}%'") 
-    elsif params[:column] == 'date_time' 
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND date_time LIKE '%#{params[:search]}%'") 
-    elsif params[:column] == 'start_location' 
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND start_location LIKE '%#{params[:search].strip}%'") 
-    elsif params[:column] == 'end_location' 
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND end_location LIKE '%#{params[:search].strip}%'") 
-    elsif params[:column] == 'free_ride' 
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND free_ride LIKE '%#{params[:search].strip}%'") 
-    elsif params[:column] == 'cancelled' 
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND cancelled LIKE '%#{params[:search].strip}%'") 
-    elsif params[:column] == 'rating' 
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND rating LIKE '%#{params[:search].strip}%'") 
-    elsif params[:allAlltype]
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}'") 
-    elsif params[:paidtype]
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND free_ride = 0") 
-    elsif params[:freetype]
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND free_ride = 1") 
-    elsif params[:cancelledtype]
-        @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND cancelled = 1") 
-    else
-        puts 'Error1'
+    if params[:search].nil? || params[:search] == "" || params[:column == "none"] then
+      if params[:column] == 'all'
+          @results = $db.execute("SELECT * FROM journeys WHERE user_id =  '#{session[:id]}'")
+      elsif params[:allAlltype]
+          @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}'") 
+      elsif params[:paidtype]
+          @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND free_ride = 0") 
+      elsif params[:freetype]
+          @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND free_ride = 1") 
+      elsif params[:cancelledtype]
+          @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND cancelled = 1") 
+      elsif params[:column] == "none" 
+          @results = ""
+      else
+          puts 'Error1'
+      end
+    else 
+       @results = $db.execute("SELECT * FROM journeys WHERE user_id = '#{session[:id]}' AND #{params[:column]} LIKE '%#{params[:search].strip}%'") 
     end
     erb :user_orders
 end
