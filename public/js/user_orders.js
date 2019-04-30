ecce138eeecf096e576c5306f701be71d072f0f3
coa18cyy <cyyuen2@sheffield.ac.uk>
2019-04-30 21:55:09 +0100

    responsive header

--- public/js/user_orders.js
+++ public/js/user_orders.js
@@ -24,35 +24,35 @@ function myFunction() {
 var x = document.getElementById("view2");
 var y = document.getElementById("view1");
 
-// function myView1() {
-//     x.style.display = "none";
-//     y.style.display = "block";
-// }
-// function myView2() {
-// 	x.style.display = "table";
-// 	y.style.display = "none";
-// }
-
-
-//Create Local Storage for table databse
-var bgColor = document.getElementById('viewButton1');
-bgColor.onclick = function() {
-    y.style.display = "block";
+function myView1() {
     x.style.display = "none";
-    localStorage.setItem('bgColor', block);
-    localStorage.setItem('bgColor', block);
-};
-bgColor.value = localStorage.getItem('bgColor');
-bgColor.onclick();
+    y.style.display = "block";
+}
+function myView2() {
+	x.style.display = "table";
+	y.style.display = "none";
+}
 
-var bgColor2 = document.getElementById('viewButton2');
-bgColor2.onclick = function() {
-      y.style.display = "none";
-      x.style.display = "table";
-      localStorage.setItem('bgColor2', block);
-};
-bgColor2.value = localStorage.getItem('bgColor2');
-bgColor2.onclick();
+
+// //Create Local Storage for table databse
+// var bgColor = document.getElementById('viewButton1');
+// bgColor.onclick = function() {
+//     y.style.display = "block";
+//     x.style.display = "none";
+//     localStorage.setItem('bgColor', block);
+//     localStorage.setItem('bgColor', block);
+// };
+// bgColor.value = localStorage.getItem('bgColor');
+// bgColor.onclick();
+
+// var bgColor2 = document.getElementById('viewButton2');
+// bgColor2.onclick = function() {
+//       y.style.display = "none";
+//       x.style.display = "table";
+//       localStorage.setItem('bgColor2', block);
+// };
+// bgColor2.value = localStorage.getItem('bgColor2');
+// bgColor2.onclick();
 
 //User manual ratings
 var a = document.getElementById("rate1");
@@ -134,13 +134,16 @@ if(lastSelected) {
 
 select.onchange = function () {
    lastSelected = select.options[select.selectedIndex].value;
-  //  console.log(lastSelected);
+   console.log(lastSelected);
    localStorage.setItem('select', lastSelected);
 }
 
 //Stay on table after submitted form
-function goToTable(reload)
-   {
-    window.location.hash = '#tableSection';
-    window.location.reload(true);
-   }
+// function goToTable(reload)
+//    {
+//     window.location.hash = '#tableSection';
+//     window.location.reload(true);
+//    }
+  window.location.hash = '#tableSection';
+
+   
