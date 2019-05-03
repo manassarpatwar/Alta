7ea14fd0e99d47d7899edf005f05e0b876b4a5f9
Tom Tonner <tbtonner1@sheffield.ac.uk>
2019-05-03 15:01:45 +0100

    new method location

---
+++ methods.rb
@@ -0,0 +1,55 @@
+
+def gather_taxis
+  if session[:admin_sheffield] then
+     @availableTaxis = $db.execute %{SELECT * FROM taxis WHERE city IS "SHEFFIELD" AND available IS "1"} #Gather all taxis
+     @unavailableTaxis = $db.execute %{SELECT * FROM taxis WHERE city IS "SHEFFIELD" AND available IS "0"} #Gather all taxis
+  elsif session[:admin_manchester] then
+     @availableTaxis = $db.execute %{SELECT * FROM taxis WHERE city IS "MANCHESTER" AND available IS "1"} #Gather all taxis
+     @unavailableTaxis = $db.execute %{SELECT * FROM taxis WHERE city IS "MANCHESTER" AND available IS "0"} #Gather all taxis
+  end
+end
+
+def add_feedback(j_id, u_id, fdbk, rat)
+  	#get feedback table from the database
+    @feedbackInfo = $db.execute("SELECT * FROM feedback")
+
+    @submitted = true
+
+    #sanitize values
+    @journey_id = j_id
+    @user_id = u_id
+    @date_time = Time.now.strftime("%Y/%m/%d %H:%M").to_s
+    @feedback = fdbk
+    @rating = rat
+
+    # perform validation
+    @journey_id_ok = isPositiveNumber? (@journey_id)
+    @feedback_ok = !@feedback.nil? && @feedback != ""
+    @user_id_ok = !@user_id.nil? && @user_id != ""
+    @rating_ok = @rating == '' || @rating == '0' || @rating == '1' || @rating == '2' || @rating == '3' || @rating == '4' || @rating == '5'
+
+
+    @all_ok = @journey_id_ok && @feedback_ok && @user_id_ok && @rating_ok
+
+    @id = @feedbackInfo[@feedbackInfo.length-1][0].to_i + 1
+
+    # add data to the database
+    if @all_ok
+        # do the insert
+        $db.execute('INSERT INTO feedback VALUES (?, ?, ?, ?, ?, ?)', [@id, @journey_id, @user_id, @date_time, @feedback, @rating])
+    end
+end
+
+def isPositiveNumber? string
+  begin
+      Float(string)
+      int = string.to_i
+      if int >= 0
+          return true
+      else
+          return false
+      end
+  rescue
+      return false
+  end
+end
